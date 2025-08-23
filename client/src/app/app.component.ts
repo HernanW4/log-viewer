1a650d736ee9b0b5cf411932fc284e9279fd5bcb
Walter Hernandez <zhernandezwalter@gmail.com>
2025-08-23 13:29:58 -0400

    feat: aa simple ui that shows the logs from server and color code based on log level

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LogViewerComponent } from './components/log-viewer/log-viewer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LogViewerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'log-viewer-client';
 }
+
