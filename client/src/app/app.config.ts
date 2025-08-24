533cc8b2def7945b43912a162df073c451fc60c0
Walter Hernandez <zhernandezwalter@gmail.com>
2025-08-23 20:32:04 -0400

    feat: Now the client code uses environment variable and config.json for sepcifying the websocket URL, although its the same for this project. It's a nice addition for prod development

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { SettingsService } from './services/settings.service';
+import { HttpClientModule } from '@angular/common/http';
 
+
+export function initializeApp(settingService: SettingsService){
+  return () => settingService.loadAppConfig();
+}
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(HttpClientModule),
+    SettingsService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeApp,
+      deps: [SettingsService],
+      multi: true
+    }
+  ]
 };
